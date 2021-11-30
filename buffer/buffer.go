e1a053f55d861d840b35c3bbfc842935210005e2
susji <susji@protonmail.com>
2021-11-30 16:01:08 +0200

    buffer: Insertion and Deletion

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -1,6 +1,10 @@
 package buffer
 
-import "github.com/susji/ked/gapbuffer"
+import (
+	"fmt"
+
+	"github.com/susji/ked/gapbuffer"
+)
 
 type Buffer struct {
 	lines []*gapbuffer.GapBuffer
@@ -8,10 +12,32 @@ type Buffer struct {
 
 func New(rawlines [][]rune) *Buffer {
 	ret := &Buffer{}
+	ret.lines = []*gapbuffer.GapBuffer{}
 	for _, rawline := range rawlines {
 		ret.lines = append(ret.lines, gapbuffer.NewFrom(rawline))
 	}
 	return ret
+}
+
+func (b *Buffer) NewLine(pos int) {
+	if pos < 0 || len(b.lines) < pos {
+		panic(fmt.Sprintf("NewLine: invalid pos=%d", pos))
+	}
+
+	left := b.lines[:pos]
+	right := b.lines[pos:]
+	b.lines = append(left, gapbuffer.New(gapbuffer.DEFAULTSZ))
+	b.lines = append(b.lines, right...)
+}
+
+func (b *Buffer) DeleteLine(pos int) {
+	if pos < 0 || len(b.lines) < pos {
+		panic(fmt.Sprintf("DeleteLine: invalid pos=%d", pos))
+	}
+	left := b.lines[:pos]
+	right := b.lines[pos+1:]
+	b.lines = append(left, right...)
+
 }
 
 func (b *Buffer) Lines() []*gapbuffer.GapBuffer {
--- buffer/buffer_test.go
+++ buffer/buffer_test.go
@@ -9,6 +9,14 @@ import (
 	ta "github.com/susji/ked/internal/testutil"
 )
 
+func bufferToRunes(buf *buffer.Buffer) [][]rune {
+	ret := [][]rune{}
+	for _, bufline := range buf.Lines() {
+		ret = append(ret, bufline.Get())
+	}
+	return ret
+}
+
 func TestSanity(t *testing.T) {
 	lines := strings.Split(`Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 sed do eiusmod tempor incididunt ut labore et dolore
@@ -29,7 +37,67 @@ laborum.
 
 	for i, gotline := range gotlines {
 		wantline := lines[i]
-		ta.Assert(t, reflect.DeepEqual(string(gotline.Get()), wantline), "unexpected got=%q, want=%q", string(gotline.Get()), wantline)
+		ta.Assert(t, reflect.DeepEqual(string(gotline.Get()), wantline),
+			"unexpected got=%q, want=%q", string(gotline.Get()), wantline)
 	}
+}
+
+func TestInsertDelete(t *testing.T) {
+	b := buffer.New(nil)
+
+	ta.Assert(t, len(b.Lines()) == 0, "should have zero lines")
+
+	//
+	// Insert one line and keep it empty.
+	//
+	b.NewLine(0)
+	got := b.Lines()
+	ta.Assert(t, len(got) == 1, "should have one line")
+	want := []rune{}
+	line := got[0]
+	ta.Assert(t, reflect.DeepEqual(line.Get(), want), "should be empty")
+
+	//
+	// Insert some text into our line.
+	//
+	msg := []rune("these are the new line contents!")
+	wantlines := [][]rune{msg}
+	line.Insert(msg)
+
+	gotlines := bufferToRunes(b)
+	ta.Assert(t, reflect.DeepEqual(gotlines, wantlines),
+		"should have updated contents, got %q", gotlines)
+
+	//
+	// Insert another line of text.
+	//
+	msg2 := []rune("We have some more text incoming!")
+	wantlines2 := [][]rune{msg, msg2}
+	b.NewLine(1)
+	b.Lines()[1].Insert(msg2)
+
+	gotlines2 := bufferToRunes(b)
+	ta.Assert(t, reflect.DeepEqual(gotlines2, wantlines2),
+		"unexpected line contents: %q", gotlines2)
+
+	//
+	// Delete first line and make sure we have the second still.
+	//
+	b.DeleteLine(0)
+	wantlines3 := [][]rune{msg2}
+
+	gotlines3 := bufferToRunes(b)
+	ta.Assert(t, reflect.DeepEqual(gotlines3, wantlines3),
+		"unexpected line contents: %q", gotlines3)
+
+	//
+	// Delete second line and make sure the buffer is empty again.
+	//
+	b.DeleteLine(0)
+	wantlines4 := [][]rune{}
+
+	gotlines4 := bufferToRunes(b)
+	ta.Assert(t, reflect.DeepEqual(gotlines4, wantlines4),
+		"should be empty, got %q", gotlines4)
 
 }
