15476cb525cda6d009f5bd75a59317bd593e95d4
susji <susji@protonmail.com>
2021-12-19 11:16:17 +0200

    ui/dialog: Asking simple questions

---
+++ ui/dialog/dialog.go
@@ -0,0 +1,41 @@
+package dialog
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+type Dialog struct {
+	prompt string
+}
+
+func New(prompt string) *Dialog {
+	return &Dialog{prompt: prompt}
+}
+
+func (d *Dialog) draw(s tcell.Screen, prompt []rune, col, lineno int) {
+	w, _ := s.Size()
+	for i := col; i < w; i++ {
+		s.SetContent(i, lineno, ' ', nil, tcell.StyleDefault)
+	}
+	for i, r := range prompt {
+		s.SetContent(col+i, lineno, r, nil, tcell.StyleDefault)
+	}
+	s.ShowCursor(len(prompt), lineno)
+	s.Show()
+}
+
+func (d *Dialog) Ask(s tcell.Screen, col, lineno int) (tcell.Key, rune) {
+	prompt := []rune(d.prompt)
+	d.draw(s, prompt, col, lineno)
+
+	for {
+		ev := s.PollEvent()
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			log.Printf("[dialog, EventKey] %s (mods=%X)\n", ev.Name(), ev.Modifiers())
+			return ev.Key(), ev.Rune()
+		}
+	}
+}
