e43c4d0d3a1812cd2fe8283d644762006aaf9236
susji <susji@protonmail.com>
2022-01-15 12:55:48 +0200

    library: Browsing files with a closure

--- library/library.go
+++ library/library.go
@@ -3,31 +3,75 @@
 package library
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
+	"log"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/susji/ked/config"
 )
 
-var lib = New()
+var (
+	ErrorMaxFiles = errors.New("maximum amount of files encountered")
+	lib           = New()
+)
 
 func New() *Library {
-	return &Library{}
+	return &Library{
+		libfs:     os.DirFS,
+		maxfiles:  config.MAXFILES,
+		filepaths: map[string]struct{}{},
+	}
 }
 
 type Library struct {
-	m    sync.RWMutex
-	dirs []string
+	libfs     func(string) fs.FS
+	maxfiles  int
+	m         sync.RWMutex
+	filepaths map[string]struct{}
 }
 
-func (l *Library) Update() {
+type Walker func(filepath string) error
+
+func (l *Library) Reset() {
+	l.filepaths = map[string]struct{}{}
+}
+
+func (l *Library) update(absdir string) error {
 	l.m.Lock()
 	defer l.m.Unlock()
+	return fs.WalkDir(l.libfs(absdir), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Printf("library update failed: %v\n", err)
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if len(l.filepaths) >= config.MAXFILES {
+			return ErrorMaxFiles
+		}
+		l.filepaths[filepath.Join(absdir, path)] = struct{}{}
+		return nil
+	})
 }
 
-func (l *Library) Add(dirpath string) {
+func (l *Library) Add(dirpath string) error {
 	abs, err := filepath.Abs(dirpath)
 	if err != nil {
 		panic(fmt.Sprintf("library Add panic: %v", err))
 	}
-	l.dirs = append(l.dirs, abs)
+	return l.update(abs)
+}
+
+func (l *Library) Walk(fn Walker) error {
+	l.m.RLock()
+	for filepath, _ := range l.filepaths {
+		fn(filepath)
+	}
+	defer l.m.RUnlock()
+	return nil
 }
--- library/library_test.go
+++ library/library_test.go
@@ -1,13 +1,41 @@
 package library_test
 
 import (
+	"os"
+	"path"
+	"reflect"
 	"testing"
 
+	"github.com/susji/ked/internal/testutil"
 	"github.com/susji/ked/library"
 )
 
 func TestBasic(t *testing.T) {
 	l := library.New()
-	l.Update()
-	l.Add(".")
+	if err := l.Add("testdata"); err != nil {
+		t.Fatal("add error: ", err)
+	}
+
+	wd, _ := os.Getwd()
+	d := path.Join(wd, "testdata")
+	want := map[string]bool{
+		path.Join(d, "a", "a.txt"):        true,
+		path.Join(d, "a", "aa", "aa.txt"): true,
+		path.Join(d, "b", "b.txt"):        true,
+		path.Join(d, "c", "c.txt"):        true,
+	}
+
+	got := map[string]bool{}
+	l.Walk(func(filename string) error {
+		t.Logf("got filename: %s\n", filename)
+		got[filename] = true
+		return nil
+	})
+
+	testutil.Assert(
+		t,
+		reflect.DeepEqual(want, got),
+		"unexpected got: %#v, want %#v",
+		got,
+		want)
 }
